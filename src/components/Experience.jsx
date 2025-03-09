83d83310c477c70d4f8c755aac61db5a604ff77b
Oskari Pessinen <oskari.pessinen@gmail.com>
2025-03-09 19:32:13 +0200

    ui changes

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -7,16 +7,16 @@ import { ReactComponent as Circlecheck } from '../assets/circle-check-regular.sv
 const Experience = () => {
     return (
         <div className="experience-container">
-            <h2 style={{fontFamily: 'Poppins', fontSize: 18, fontWeight: 500, color: '#525150'}}>My</h2>
+            <h2 style={{fontFamily: 'Poppins', fontSize: 18, fontWeight: 500, color: '#525150',}}>My</h2>
             <h1 style={{fontFamily: 'Poppins', fontSize: 42, fontWeight: 600}}>Experience</h1>
             <div className="experience-grid">
                 <div className="experience-card">
                     <h3>Frontend</h3>
                     <ul className="skills-list">
-                        <li><Circlecheck className="skill-icon" />React</li>
-                        <li><Circlecheck className="skill-icon" />React Native</li>
                         <li><Circlecheck className="skill-icon" />JavaScript</li>
                         <li><Circlecheck className="skill-icon" />TypeScript</li>
+                        <li><Circlecheck className="skill-icon" />React</li>
+                        <li><Circlecheck className="skill-icon" />React Native</li>
                         <li><Circlecheck className="skill-icon" />HTML5</li>
                         <li><Circlecheck className="skill-icon" />CSS</li>
                     </ul>
@@ -25,11 +25,11 @@ const Experience = () => {
                     <h3>Backend</h3>
                     <ul className="skills-list">
                         <li><Circlecheck className="skill-icon" />Python</li>
-                        <li><Circlecheck className="skill-icon" />Java</li>
+                        <li><Circlecheck className="skill-icon" />MongoDB</li>
                         <li><Circlecheck className="skill-icon" />Flask</li>
                         <li><Circlecheck className="skill-icon" />Node.js</li>
                         <li><Circlecheck className="skill-icon" />SQL</li>
-                        <li><Circlecheck className="skill-icon" />MongoDB</li>
+                        <li><Circlecheck className="skill-icon" />Java</li>
                     </ul>
                 </div>
             </div>

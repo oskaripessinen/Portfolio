e6f395225c12a6f4072ff1ba46ddd7f314aa8393
oskaripessinen <oskari.pessinen@gmail.com>
2025-12-30 16:23:54 +0200

    header animation

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -32,9 +32,9 @@ export function Navbar() {
 
   return (
     <motion.header 
-      initial={{ y: -70, opacity: 0 }}
-      animate={{ y: 0, opacity: 1 }}
-      transition={{ duration: 0.6, ease: "easeOut" }}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1, ease: "easeOut" }}
       className="sticky top-0 z-50 w-full px-4 md:px-10 border-b bg-background/80 backdrop-blur-md supports-backdrop-filter:bg-background/60"
     >
         <div className="flex h-16 items-center justify-between">

cc1411c802380d561b5f63a28e2582c7f3e17183
oskaripessinen <oskari.pessinen@gmail.com>
2026-01-03 18:33:47 +0200

    metadata

---
+++ app/opengraph-image.tsx
@@ -0,0 +1,25 @@
+import { ImageResponse } from 'next/og'
+
+export const alt = 'Oskari Pessinen | Portfolio'
+export const size = { width: 1200, height: 630 }
+export const contentType = 'image/png'
+
+export default function Image() {
+  return new ImageResponse(
+    (
+      <div tw="flex flex-col items-center justify-center w-full h-full bg-white">
+        <div tw="flex flex-col items-center">
+          <h1 
+            tw="text-7xl font-black text-black tracking-tighter"
+            style={{ fontFamily: 'sans-serif', fontWeight: '900' }}
+          >
+            Oskari Pessinen
+          </h1>
+          
+        </div>
+
+      </div>
+    ),
+    { ...size }
+  )
+}
